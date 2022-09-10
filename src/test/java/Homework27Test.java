94c93bd9cfcb6b0156bc384cf0cc8b93623702ac
YanaOsipova <yana.osipova.prum@gmail.com>
2022-09-10 16:44:54 +0700

    homework_27: сделала правки по замечаниям

--- src/test/java/Homework27Test.java
+++ src/test/java/Homework27Test.java
@@ -1,41 +1,62 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class Homework27Test {
     static final Logger userLogger = LogManager.getLogger(Homework27Test.class);
 
     private WebDriver driver;
 
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
     @Test
-    public void test_1() {
+    @Order(1)
+    public void testFieldFilling() {
         userLogger.info("Начало теста 1");
         var login = System.getProperty("login");
         var password = System.getProperty("password");
         driver = ConfigWebDriver.getDriver();
         driver.manage().window().maximize();
+        var webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(3L));
         driver.get("https://otus.ru");
         driver.findElement(By.cssSelector("button.header2__auth")).click();
-        Util.waitSecond(1);
 
-        var element = driver.findElement(By.xpath("//form[@action = '/login/']//input[@name = 'email']"));
+        var form = webDriverWait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form[@action = '/login/']")));
+        var element = form.findElement(By.xpath(".//input[@name = 'email']"));
         element.clear();
         element.sendKeys(login);
-
-        element = driver.findElement(By.xpath("//form[@action = '/login/']//input[@name = 'password']"));
+        element = form.findElement(By.xpath(".//input[@name = 'password']"));
         element.clear();
         element.sendKeys(password);
+        form.findElement(By.xpath(".//button[@type = 'submit']")).submit();
 
-        driver.findElement(By.xpath("//form[@action = '/login/']//button[@type = 'submit']")).submit();
+        var action = new Actions(driver);
+        form = webDriverWait
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.cssSelector("div.header2-menu__item.header2__right__menu__item.header2-menu__item_small.header2-menu__item_dropdown.header2-menu__item_dropdown_no-border")));
+        action.moveToElement(form).perform();
+        webDriverWait
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.cssSelector("a[title='Личный кабинет']")))
+                .click();
+        webDriverWait
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.cssSelector("a[title='О себе']"))).click();
 
-        Util.waitSecond(1);
-        driver.get("https://otus.ru/lk/biography/personal/");
-        Util.waitSecond(1);
         element = driver.findElement(By.id("id_fname"));
         element.clear();
         element.sendKeys("Тест");
@@ -60,50 +81,58 @@ public class Homework27Test {
         element.clear();
         element.sendKeys("02.02.2002");
 
-        driver.findElement(By.xpath("//div[@data-slave-selector='.js-lk-cv-dependent-slave-city']")).click();
-        driver.findElement(By.xpath("//button[@title='Украина']")).click();
+        driver.findElement(By.xpath("//div[@data-slave-selector='.js-lk-cv-dependent-slave-city']"))
+                .click();
+        webDriverWait
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.xpath("//button[@title='Украина']")))
+                .click();
 
-        Util.waitSecond(3);
-
-        driver.findElements(By.xpath("//div[@data-selected-option-class='lk-cv-block__select-option_selected']")).get(1).click();
-        driver.findElement(By.xpath("//button[@title='Мариуполь']")).click();
-
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='placeholder']")))
+                .click();
+        webDriverWait.until(ExpectedConditions
+                        .visibilityOfElementLocated(By.xpath("//button[@title='Мариуполь']")))
+                .click();
         driver.findElements(By.xpath("//div[@data-selected-option-class='lk-cv-block__select-option_selected']")).get(2).click();
         driver.findElement(By.xpath("//button[@title='Начальный уровень (Beginner)']")).click();
 
         driver.findElements(By.xpath("//span[@class='radio__label']"))
                 .get(1)
                 .click();
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[1]/div/div[1]/div[5]/div[2]/div[1]/div[2]/label/span"))
-                .click();
 
-        if (!driver.findElement(By.xpath("//*[@id='id_is_email_preferable']"))
-                .getAttribute("checked").equalsIgnoreCase("true")) {
-            driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[2]/div[1]/div[2]/div/div[2]/div[2]/label/span"))
+        if (!driver.findElement(By.xpath("//input[@title='Полный день' and @checked]")).getAttribute("checked").equalsIgnoreCase("true")) {
+            driver.findElement(By.xpath("//span[contains(text(), 'Полный день')]"))
                     .click();
         }
-
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[2]/div[2]/div/div/div[1]/div/div/div"))
+        if (!driver.findElement(By.xpath("//*[@id='id_is_email_preferable']"))
+                .getAttribute("checked").equalsIgnoreCase("true")) {
+            driver.findElements(By.xpath("//span[contains(text(), 'Предпочтительный способ связи')]"))
+                    .get(0)
+                    .click();
+        }
+        var elements = driver.findElements(By.xpath("//button[contains(text(), 'Удалить')]"));
+        for (int i = 1; i < elements.size(); i += 2) {
+            elements.get(i).click();
+        }
+        driver.findElement(By.xpath("//button[contains(text(), 'Добавить')]"))
                 .click();
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[2]/div[2]/div/div/div[1]/div/div/div/div/div/button[5]"))
+        driver.findElement(By.xpath("//span[contains(text(), 'Способ связи')]"))
                 .click();
-        element = driver.findElement(By.xpath("//*[@id='id_contact-0-value']"));
-        element.clear();
+        driver.findElements(By.xpath("//button[contains(text(), 'Skype')]")).get(2).click();
+        element = driver.findElement(By.xpath("//*[@id='id_contact-2-value']"));
         element.sendKeys("test@mail.com");
 
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[2]/div[2]/button"))
+        driver.findElement(By.xpath("//button[contains(text(), 'Добавить')]"))
                 .click();
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[2]/div[2]/div/div[2]/div[1]/div/div/div/label/div"))
+        driver.findElement(By.xpath("//span[contains(text(), 'Способ связи')]"))
                 .click();
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div/div/button[3]"))
-                .click();
-        element = driver.findElement(By.xpath("//*[@id='id_contact-1-value']"));
-        element.clear();
-        element.sendKeys("vk.com/test");
+        driver.findElements(By.xpath("//button[contains(text(), 'VK')]")).get(3).click();
+        element = driver.findElement(By.xpath("//*[@id='id_contact-3-value']"));
+        element.sendKeys("vk.com/onaosipova");
 
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[4]/div[1]/div[1]/div"))
+        driver.findElement(By.xpath("//div[@class='select select_full']"))
                 .click();
-        driver.findElement(By.xpath("//*[@id='id_gender']/option[3]"))
+        driver.findElement(By.xpath("//*[@id='id_gender']/option[@value='f']"))
                 .click();
 
         element = driver.findElement(By.xpath("//*[@id='id_company']"));
@@ -114,34 +143,46 @@ public class Homework27Test {
         element.clear();
         element.sendKeys("qa engineer");
 
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[2]/div/div/button[1]"))
+        driver.findElement(By.xpath("//button[@title='Сохранить и продолжить']"))
                 .click();
         userLogger.info("Конец теста 1");
     }
 
     @Test
-    public void test_2() {
+    @Order(2)
+    public void testCheckingFields() {
         userLogger.info("Начало теста 2");
         var login = System.getProperty("login");
         var password = System.getProperty("password");
         driver = ConfigWebDriver.getDriver();
         driver.manage().window().maximize();
+        var webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(3L));
         driver.get("https://otus.ru");
         driver.findElement(By.cssSelector("button.header2__auth")).click();
-        Util.waitSecond(1);
 
-        var element = driver.findElement(By.xpath("//form[@action = '/login/']//input[@name = 'email']"));
+        var form = webDriverWait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form[@action = '/login/']")));
+        var element = form.findElement(By.xpath(".//input[@name = 'email']"));
         element.clear();
         element.sendKeys(login);
-
-        element = driver.findElement(By.xpath("//form[@action = '/login/']//input[@name = 'password']"));
+        element = form.findElement(By.xpath(".//input[@name = 'password']"));
         element.clear();
         element.sendKeys(password);
+        form.findElement(By.xpath(".//button[@type = 'submit']")).submit();
 
-        driver.findElement(By.xpath("//form[@action = '/login/']//button[@type = 'submit']")).submit();
+        var action = new Actions(driver);
+        form = webDriverWait
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.cssSelector("div.header2-menu__item.header2__right__menu__item.header2-menu__item_small.header2-menu__item_dropdown.header2-menu__item_dropdown_no-border")));
+        action.moveToElement(form).perform();
+        webDriverWait
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.cssSelector("a[title='Личный кабинет']")))
+                .click();
+        webDriverWait
+                .until(ExpectedConditions
+                        .visibilityOfElementLocated(By.cssSelector("a[title='О себе']"))).click();
 
-        Util.waitSecond(1);
-        driver.get("https://otus.ru/lk/biography/personal/");
         var text = driver.findElement(By.id("id_fname")).getAttribute("value");
         assertThat(text).as("Name").isEqualTo("Тест");
         text = driver.findElement(By.id("id_fname_latin")).getAttribute("value");
@@ -152,22 +193,22 @@ public class Homework27Test {
         assertThat(text).as("Last Name(Latin)").isEqualTo("Testov");
         text = driver.findElement(By.id("id_blog_name")).getAttribute("value");
         assertThat(text).as("Last Name(Latin)").isEqualTo("TestBlogov");
-        text = driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[1]/div/div[4]/div/div/input"))
+        text = driver.findElement(By.xpath("//input[@name='date_of_birth']"))
                 .getAttribute("value");
         assertThat(text).as("Date").isEqualTo("02.02.2002");
-        text = driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[1]/div/div[1]/div[1]/div[2]/div/label/div"))
-                .getText();
-        assertThat(text).as("Country").isEqualTo("Украина");
-        text = driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[1]/div/div[1]/div[2]/div[2]/div/label/div"))
-                .getText();
-        assertThat(text).as("City").isEqualTo("Мариуполь");
-        text = driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[1]/div/div[1]/div[3]/div[2]/div/label/div"))
-                .getText();
-        assertThat(text).as("City").isEqualTo("Начальный уровень (Beginner)");
+        text = driver.findElement(By.xpath("//input[@name='country']"))
+                .getAttribute("value");
+        assertThat(text).as("Country").isEqualTo("4");
+        text = driver.findElement(By.xpath("//input[@data-title='Город']"))
+                .getAttribute("value");
+        assertThat(text).as("City").isEqualTo("87");
+        text = driver.findElement(By.xpath("//input[@data-title='Уровень знания английского языка']"))
+                .getAttribute("value");
+        assertThat(text).as("City").isEqualTo("1");
         text = driver.findElement(By.xpath("//*[@id='id_ready_to_relocate_1']"))
                 .getAttribute("value");
         assertThat(text).as("Checkbox").isEqualTo("True");
-        text = driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[1]/div/div[1]/div[5]/div[2]/div[1]/div[2]/label/input"))
+        text = driver.findElement(By.xpath("//input[@title='Полный день']"))
                 .getAttribute("checked");
         assertThat(text).as("Checkbox").isEqualTo("true");
         text = driver.findElement(By.xpath("//*[@id='id_email']"))
@@ -176,19 +217,22 @@ public class Homework27Test {
         text = driver.findElement(By.xpath("//*[@id='id_is_email_preferable']"))
                 .getAttribute("checked");
         assertThat(text).as("Checkbox").isEqualTo("true");
+
         text = driver.findElement(By.xpath("//*[@id='id_contact-0-value']"))
                 .getAttribute("value");
         assertThat(text).as("Skype").isEqualTo("test@mail.com");
-        text = driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[2]/div[2]/div/div[1]/div[1]/div/div/div/label/div"))
-                .getText();
-        assertThat(text).as("Skype").isEqualTo("Skype");
+        text = driver.findElement(By.xpath("//input[@value='skype']"))
+                .getAttribute("value");
+        assertThat(text).as("Skype").isEqualTo("skype");
+
         text = driver.findElement(By.xpath("//*[@id='id_contact-1-value']"))
                 .getAttribute("value");
-        assertThat(text).as("VK").isEqualTo("vk.com/test");
-        text = driver.findElement(By.xpath("/html/body/div[1]/div/div[4]/div[3]/div[2]/div[2]/div/form/div[1]/div[3]/div[2]/div[2]/div/div[2]/div[1]/div/div/div/label/div"))
-                .getText();
-        assertThat(text).as("VK").isEqualTo("VK");
-        text = driver.findElement(By.xpath("//*[@id=\"id_gender\"]/option[3]"))
+        assertThat(text).as("VK").isEqualTo("vk.com/onaosipova");
+        text = driver.findElement(By.xpath("//input[@value='vk']"))
+                .getAttribute("value");
+        assertThat(text).as("VK").isEqualTo("vk");
+
+        text = driver.findElement(By.xpath("//*[@id='id_gender']/option[@value='f']"))
                 .getAttribute("selected");
         assertThat(text).as("checkbox").isEqualTo("true");
         text = driver.findElement(By.xpath("//*[@id='id_company']"))
@@ -203,6 +247,8 @@ public class Homework27Test {
     @AfterEach
     public void close() {
         userLogger.info("Закрытие драйвера");
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
